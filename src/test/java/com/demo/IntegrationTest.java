54cf2dd87dbd805230fb3839fe38af8d079760b9
ankth <anhkth@softdreams.vn>
2022-12-12 14:42:43 +0700

    Initial version of DemoHAnh generated by JHipster-7.0.1

---
+++ src/test/java/com/demo/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import com.demo.DemoHAnhApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = DemoHAnhApp.class)
+public @interface IntegrationTest {
+}
